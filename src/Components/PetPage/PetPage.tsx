9c4453645c84f6118e80c335fe77a4677ab7cd9d
Asad Jawaid <asazycat@gmail.com>
2025-02-28 12:03:55 +0000

    dynamic routes for each pet

--- src/Components/PetPage/PetPage.tsx
+++ src/Components/PetPage/PetPage.tsx
@@ -1,10 +1,25 @@
 
+import { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
+import { apiKeyProvider } from "../../Contexts/Access_Token_Context";
+import { Pet } from "../../Interfaces";
 export default function PetPage() {
+
     const {id} = useParams();
+    const {token_type, access_token} = useContext(apiKeyProvider)
+    const [pet, setPet] = useState<Pet>({})
+    useEffect(() => {
+       if(access_token != "") {(async function() {
+            const res = await fetch(`https://api.petfinder.com/v2/animals/${id}`, {headers: {
+                    Authorization: `${token_type} ${access_token}`
+                }})
+            return res
+        })().then((res) => res.json()).then((res) =>setPet(res.animal))
+    }},[access_token, id, token_type])
+
     return (
         <>
-        <h1>{id}</h1>
+        <h1>{JSON.stringify(pet)}</h1>
         </>
     )
 }
