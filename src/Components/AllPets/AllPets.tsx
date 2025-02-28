9997995390dc8b18398279dd464614baebce0658
Asad Jawaid <asazycat@gmail.com>
2025-02-28 19:43:35 +0000

    useMemo to optimize rendering for Pets page

--- src/Components/AllPets/AllPets.tsx
+++ src/Components/AllPets/AllPets.tsx
@@ -2,33 +2,32 @@ import { useContext, useEffect, useState, useMemo } from "react";
 import { apiKeyProvider } from "../../Contexts/Access_Token_Context";
 import { Pet } from "../../Interfaces";
 import EachPet from "./EachPet";
-
+import { useRenderCount } from "@uidotdev/usehooks";
 export default function AllPets() {
-   
+    const renderCount = useRenderCount();
     const [allPets, setAllPets] = useState<Pet[]>([])
     const {token_type, access_token} = useContext(apiKeyProvider)
+    
     const retrievePets = useMemo(async () => {
-            const res_1 = await (async function () {
+       return (await (async function () {
             const res = await fetch("https://api.petfinder.com/v2/animals", {
                 headers: {
                     Authorization: `${token_type} ${access_token}`
                 }
             });
             return res;
-        })();
-        const res_2 = await res_1.json();
-        return res_2.animals;
-       
-    },[access_token, token_type])
+        })()).json();
+        
+},[access_token, token_type])
 
     useEffect(() => {
         (async function () {
          const resolved = await Promise.resolve(retrievePets)
-         setAllPets(resolved)
+         setAllPets(resolved.animals)
         })()
     }, [access_token, retrievePets, token_type])
-  
-
+    console.log(renderCount)
+    
     return (
         <ul>
             {allPets.map((pet) => <li key={pet.id}><EachPet pet={pet}/></li>)}
