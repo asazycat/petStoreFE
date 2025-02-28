f7426c02506865e82311552e9194dc913a6a654f
Asad Jawaid <asazycat@gmail.com>
2025-02-28 19:22:01 +0000

    useMemo to optimize rendering for Pets page

--- src/Components/AllPets/AllPets.tsx
+++ src/Components/AllPets/AllPets.tsx
@@ -2,13 +2,11 @@ import { useContext, useEffect, useState, useMemo } from "react";
 import { apiKeyProvider } from "../../Contexts/Access_Token_Context";
 import { Pet } from "../../Interfaces";
 import EachPet from "./EachPet";
-import { useRenderCount } from "@uidotdev/usehooks";
 
 export default function AllPets() {
    
     const [allPets, setAllPets] = useState<Pet[]>([])
     const {token_type, access_token} = useContext(apiKeyProvider)
-    const renderCount = useRenderCount();
     const retrievePets = useMemo(async () => {
             const res_1 = await (async function () {
             const res = await fetch("https://api.petfinder.com/v2/animals", {
@@ -30,7 +28,7 @@ export default function AllPets() {
         })()
     }, [access_token, retrievePets, token_type])
   
-    console.log(renderCount)
+
     return (
         <ul>
             {allPets.map((pet) => <li key={pet.id}><EachPet pet={pet}/></li>)}
